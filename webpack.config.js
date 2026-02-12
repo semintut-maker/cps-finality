f661952840f412f028f5f1452530a5a7514d76e1
semin <semin.tut@gmail.com>
2026-02-12 15:33:43 +0500

    исправил ошибки

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const publicUrl = process.env.PUBLIC_URL || ''
 
 module.exports = {
   mode: 'production',
